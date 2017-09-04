f5ad63b99bf7296600eb4eb5f0edf5485348a2a5
Omran Jamal <o.jamal97@gmail.com>
2017-09-04 23:12:04 +0600

    Working Build

--- build.js
+++ build.js
@@ -3,11 +3,12 @@
 const fs = require('fs');
 const browserify = require('browserify');
 
-browserify({ entries: ["./scripts/try.js"] })
-    .transform("babelify", {
-        global: true,
-        only: /^.*$/,
-        presets: ["es2015", "es2016", "es2017", "react"]
-    })
-    .bundle()
-    .pipe(fs.createWriteStream("./scripts/try.js"));
+browserify({
+    entries: ["./scripts/try.js"]
+}).transform("babelify", {
+    global: true,
+    only: /^.*$/,
+    presets: ["es2015", "es2016", "es2017", "react"]
+}).transform("uglifyify", {
+    global: true
+}).bundle().pipe(fs.createWriteStream("./dist_scripts/try.js"))
